e1ee638f2eadd84ca7c97c7a10555a317d19040f
daigoyb <rafaeldaigo.yb@gmail.com>
2021-01-15 17:44:11 -0300

    correcao de erros rota /cadastro_ja

--- server/app.py
+++ server/app.py
@@ -14,38 +14,32 @@ def cadastro_ja():
     if request.method == 'POST':
         data = request.get_json()
         print(data)
-        if os.path.isfile('db/cadastro_ja.json'):
-            with open('db/cadastro_ja.json', 'a', encoding='utf-8') as ja_file:
-                json.dump({data['cpf']: {
-                    "name": data['name'],
-                    "endereco": data['endereco'],
-                    "estado": data['estado'],
-                    "area_interesse": data['area_interesse'],
-                    "genero": data['identificacao_genero_sex'],
-                    "identificacao_comunidade": {
-                        "sn": data['identificacao_comunidade']['sn'],
-                        "qual_comunidade": data['identificacao_comunidade']['qual']
-                    },
-                    "etnia": data['etnia'],
-                    "habilidades": data['habilidades']
-                }},ja_file, ensure_ascii=False, indent=4)
+        dct_data = {data['cpf']: {
+            "name": data['name'],
+            "endereco": data['endereco'],
+            "estado": data['estado'],
+            "area_interesse": data['area_interesse'],
+            "genero": data['identificacao_genero_sex'],
+            "identificacao_comunidade": {
+                "sn": data['identificacao_comunidade']['sn'],
+                "qual_comunidade": data['identificacao_comunidade']['qual']
+            },
+            "etnia": data['etnia'],
+            "habilidades": data['habilidades']
+        }}
+        if os.path.isfile('db/cadastro_ja.json'): # se o arquivo já existir
+            with open('db/cadastro_ja.json', 'r', encoding='utf-8') as ja_file:
+                json_data = json.load(ja_file)
                 ja_file.close()
-                return 'Cadastrado'
-        else:        
+            json_data.update(dct_data)
             with open('db/cadastro_ja.json', 'w', encoding='utf-8') as ja_file:
-                json.dump({data['cpf']: {
-                    "name": data['name'],
-                    "endereco": data['endereco'],
-                    "estado": data['estado'],
-                    "area_interesse": data['area_interesse'],
-                    "genero": data['identificacao_genero_sex'],
-                    "identificacao_comunidade": {
-                        "sn": data['identificacao_comunidade']['sn'],
-                        "qual_comunidade": data['identificacao_comunidade']['qual']
-                    },
-                    "etnia": data['etnia'],
-                    "habilidades": data['habilidades']
-                }}, ja_file, ensure_ascii=False, indent=4)
+                json.dump(json_data, ja_file, ensure_ascii=False, indent=4)
                 ja_file.close()
-                return 'Cadastrado'
+            return 'Cadastrado - doc atualizado'
+        else: # se o arquivo não existir
+            with open('db/cadastro_ja.json', 'w', encoding='utf-8') as ja_file:
+                json.dump(dct_data, ja_file, ensure_ascii=False, indent=4)
+                ja_file.close()
+            return 'Cadastrado - Arquivo criado'
     return 'nao entrou no post'
+
