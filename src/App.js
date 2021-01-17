b18757ebe6aba8f48bbe2dd294e28d5288bcf8d7
daigoyb <rafaeldaigo.yb@gmail.com>
2021-01-16 23:47:41 -0300

    usando framework react-materialize

--- src/App.js
+++ src/App.js
@@ -4,14 +4,6 @@ import LandingPage from './pages/LandingPage';
 import './App.css'; 
 
 function App() {
-  const [currentTime, setCurrentTime] = useState(0);
-
-  useEffect(() => {
-    fetch('/time').then(res => res.json()).then(data => {
-      setCurrentTime(data.time);
-    });
-  }, []); 
-
   return (
     <div className="App">
       <LandingPage />
--- src/pages/LandingPage/index.js
+++ src/pages/LandingPage/index.js
@@ -1,7 +1,29 @@
 import React from 'react';
-
-import './style.css';
+import 'materialize-css';
+import { 
+        Button, Card, Row, Col, Navbar, Container, NavItem
+    } from 'react-materialize';
 import firstPagebg from '../../Assets/Mask.png';
+
+const landingPage = () => {
+    return (
+        <>  
+            <Navbar>
+                <Col>
+                    <NavItem>Icon</NavItem>
+                </Col>
+                <Col>
+                    <NavItem>Fale Conosco</NavItem>
+                </Col>
+            </Navbar>
+            <Row>
+                <Container style={pageStyle.initContainer}>
+                    
+                </Container>
+            </Row>
+        </>
+    )
+}
 
 var sectionStyle = {
     backgroundImage: `url(${firstPagebg})`,
@@ -9,79 +31,21 @@ var sectionStyle = {
     backgroundPosition: 'fixed'
 };
 
-const landingPage = () => {
-    return (
-        <>
-            <nav>
-                <div className="nav-wrapper">
-                    <a href="#!" color="#BF360C" class="brand-logo">
-                        <p className="brand">
-                            abraba
-                        </p>
-                    </a>
-                    <ul className="right hide-on-med-and-down">
-                        <li className="li">
-                            <p>Já tem uma conta?</p>
-                            <a href="sass.html" className="bold">Entre aqui</a>
-                        </li>
-                        <li className="navButton">
-                            <a class="waves-effect waves-light btn">Comece agora</a>
-                        </li>
-                    </ul>
-                </div>
-            </nav>
-
-            <div style={sectionStyle} className="firstPage fixed">
-                <div className="d-flex">
-                    <h1>
-                        Prepare-se, jovem aprendiz
-                    </h1>
-                    <h2>
-                        Vamos te ajudar a lançar a braba e conseguir seu primeiro emprego!
-                    </h2>
-
-                    <div className="d-flex2">
-                        <a className="waves-effect waves-light btn">
-                            <p className="brand2">
-                                Começar agora
-                            </p>
-                        </a>
-                        <a>Saiba como</a>
-                    </div>
-
-                </div>
-            </div>
-
-            <div style={{ backgroundColor: '#00695C' }} className="firstPage fixed">
-                <div className="display-flex">
-                    <div className="display-flex">
-                        <h1 style={{ overflowX: 'hidden' }}>
-                            Veja como abraba promove a sua inclusão:
-                    </h1>
-                    </div>
-
-                    <div className="d-flex">
-                        <div className="card">
-                            <div className="card-reveal">
-                                <span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span>
-                                <p>Here is some more information about this product that is only revealed once clicked on.</p>
-                            </div>
-                            <div className="card-reveal">
-                                <span className="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span>
-                                <p>Here is some more information about this product that is only revealed once clicked on.</p>
-                            </div>
-                            <div className="card-reveal">
-                                <span className="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span>
-                                <p>Here is some more information about this product that is only revealed once clicked on.</p>
-                            </div>
-                        </div>
-                    </div>
-
-
-                </div>
-            </div>
-        </>
-    )
+const pageStyle = {
+    initContainer:{
+        position: 'absolute',
+        height: '564px',
+        left: '0%',
+        right: '0%',
+        top: '64px',
+        color: '#00695C'
+    },
+    navBarStyle:{
+        position: 'absolute',
+        width: '1440px',
+        height: '64px',
+        left: '0px',
+        top: '0px',
+    }
 }
-
 export default landingPage; 
